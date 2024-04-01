2325ffc11b3441ff42fa93aabe07e436ef62889f
gabrielmartinsgomes <gabrielhenrickmartind3454@gmail.com>
2024-04-01 16:46:39 -0300

    aula 01/04

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
 import { ref, reactive } from 'vue'
 
-const titulo = ref('Título');
+const titulo = ref('Título')
+const mostrarResultado = ref (false)
+
 const produto = reactive ({
   nome: 'Produto',
-  preco: 19.99,
+  preco: 19,
   quantidade: 10,
   categorias: []
 })
+
+function formatarPreco(preco) {
+  return `R$ ${preco.toFixed(2).replace('.', ',')}`
+}
 
 </script>
 
@@ -17,13 +23,30 @@ const produto = reactive ({
     <div class="formulario">
       <h2>Formulário</h2>
       <input type="text" v-model="titulo"/>
+      <form @submit.prevent="mostrarResultado = !mostrarResultado">
+      <div class="row">
+      <label for="">Nome</label>
+      <input type="text" v-model="produto.nome" required>
+      </div>
+      <div class="row">
+      <label for="">Preço</label>
+      <input type="number" v-model="produto.preco">
+      </div>
+      <div class="row">
+        <label for="">Quantidade</label>
+        <input type="number" v-model="produto.quantidade">
+      </div>
+      <button type="submit">Mostrar</button>
+      
+      </form>
     </div>
-    <div class="resultado">
+    <div v-if="mostrarResultado" class="resultado">
       <h2>Resultado</h2>
       <p>Nome: {{ produto.nome }}</p>
-      <p>Preço: {{ produto.preco }}</p>
+      <p>Preço: {{ formatarPreco(produto.preco) }}</p>
       <p>Quantidade: {{ produto.quantidade }}</p>
       <p>Categorias: {{ produto.categorias }}</p>
+      <p>{{ mostrarResultado }}</p>
     </div>
   </div>
 </template>
@@ -49,5 +72,12 @@ const produto = reactive ({
 }
 .formulario {
   background-color: #c4c500;
+}
+
+.formulario .row{
+  margin: 10px 15px;
+  width: 70%;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
